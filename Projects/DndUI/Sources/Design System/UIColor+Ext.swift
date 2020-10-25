e80f18b79e916b7d58012b382ad20684a4835d1a
Gunter <xc200925@gmail.com>
2020-10-25 19:07:39 +0900

    Create UIColor+Ext.swift

---
+++ Projects/DndUI/Sources/Design System/UIColor+Ext.swift
@@ -0,0 +1,21 @@
+//
+//  UIColor+Ext.swift
+//  DndUI
+//
+//  Created by Gunter on 2020/10/25.
+//
+
+import UIKit
+
+public extension UIColor {
+  static let gray900 = UIColor(red: 27/255, green: 28/255, blue: 29/255, alpha: 1.0)
+  static let gray800 = UIColor(red: 62/255, green: 64/255, blue: 66/255, alpha: 1.0)
+  static let gray700 = UIColor(red: 97/255, green: 101/255, blue: 104/255, alpha: 1.0)
+  static let gray600 = UIColor(red: 133/255, green: 138/255, blue: 141/255, alpha: 1.0)
+  static let gray500 = UIColor(red: 168/255, green: 174/255, blue: 179/255, alpha: 1.0)
+  static let gray400 = UIColor(red: 189/255, green: 194/255, blue: 198/255, alpha: 1.0)
+  static let gray300 = UIColor(red: 205/255, green: 209/255, blue: 212/255, alpha: 1.0)
+  static let gray200 = UIColor(red: 221/255, green: 224/255, blue: 226/255, alpha: 1.0)
+  static let gray100 = UIColor(red: 237/255, green: 239/255, blue: 240/255, alpha: 1.0)
+  static let gray000 = UIColor(red: 248/255, green: 248/255, blue: 249/255, alpha: 1.0)
+}
