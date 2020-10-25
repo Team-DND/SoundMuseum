a9cef44f41001bca8aff5731b4242aed81ef9688
Gunter <xc200925@gmail.com>
2020-10-25 18:07:37 +0900

    Templates cookiecutter 추가

---
+++ Templates/Framework/{{cookiecutter.name}}/Sources/{{cookiecutter.name}}.swift
@@ -0,0 +1,2 @@
+struct {{cookiecutter.name}} {
+}
---
+++ Templates/Framework/{{cookiecutter.name}}/Tests/{{cookiecutter.name}}Spec.swift
@@ -0,0 +1,10 @@
+import StyleShareTest
+
+@testable import {{cookiecutter.name}}
+
+final class {{cookiecutter.name}}Spec: QuickSpec {
+  override func spec() {
+    it("") {
+    }
+  }
+}
