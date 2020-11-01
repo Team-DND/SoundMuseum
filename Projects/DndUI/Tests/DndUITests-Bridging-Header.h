8f5049b125cabdbca8cbe96028ab198d8e53ace1
Gunter <xc200925@gmail.com>
2020-11-01 20:26:50 +0900

    Create DndUITests-Bridging-Header.h

---
+++ Projects/DndUI/Tests/DndUITests-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  DndUITests-Bridging-Header.h
+//  DndUITests
+//
+//  Created by Gunter on 2020/11/01.
+//
+
+#ifndef DndUITests_Bridging_Header_h
+#define DndUITests_Bridging_Header_h
+
+
+#endif /* DndUITests_Bridging_Header_h */
