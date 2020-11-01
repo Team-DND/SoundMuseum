82945f83e19cc55aea733b2e1af4ffeaa4189d41
Gunter <xc200925@gmail.com>
2020-11-01 20:26:32 +0900

    Create ImageNode.swift

---
+++ Projects/DndUI/Sources/Design System/ImageNode.swift
@@ -0,0 +1,25 @@
+//
+//  ImageNode.swift
+//  DndUI
+//
+//  Created by Gunter on 2020/11/01.
+//
+
+import AsyncDisplayKit
+
+public final class ImageNode: ASImageNode {
+  public override init() {
+    super.init()
+    self.isLayerBacked = true
+    self.backgroundColor = .white
+    self.isOpaque = true
+  }
+
+  public override var backgroundColor: UIColor? {
+    didSet {
+      var alpha: CGFloat = 0.0
+      self.backgroundColor?.getRed(nil, green: nil, blue: nil, alpha: &alpha)
+      self.isOpaque = (alpha >= 1)
+    }
+  }
+}
