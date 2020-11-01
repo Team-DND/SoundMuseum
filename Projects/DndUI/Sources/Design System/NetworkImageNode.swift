470973912ee08640e960393dc6c635e415b4a200
Gunter <xc200925@gmail.com>
2020-11-01 20:26:34 +0900

    Create NetworkImageNode.swift

---
+++ Projects/DndUI/Sources/Design System/NetworkImageNode.swift
@@ -0,0 +1,28 @@
+//
+//  NetworkImageNode.swift
+//  DndUI
+//
+//  Created by Gunter on 2020/11/01.
+//
+
+import AsyncDisplayKit
+
+public final class NetworkImageNode: ASNetworkImageNode {
+  public init() {
+    super.init(
+      cache: ASPINRemoteImageDownloader.shared(),
+      downloader: ASPINRemoteImageDownloader.shared()
+    )
+    self.isLayerBacked = true
+    self.backgroundColor = .white
+    self.isOpaque = true
+  }
+
+  public override var backgroundColor: UIColor? {
+    didSet {
+      var alpha: CGFloat = 0.0
+      self.backgroundColor?.getRed(nil, green: nil, blue: nil, alpha: &alpha)
+      self.isOpaque = (alpha >= 1)
+    }
+  }
+}
